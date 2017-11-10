56f63b660f64c4138ab87b2525bb022aa92a4483
Frank de Groot - Schouten <fjtdg@hotmail.com>
2017-11-10 13:12:43 +0100

    SPA JS refactored, setting window location hash

--- src/main/webapp/resources/js/registreer.js
+++ src/main/webapp/resources/js/registreer.js
@@ -1,96 +1,92 @@
+var h1 = document.querySelector('h1');
+var errorLabel = document.querySelector('.label-important');
+var activatieformulier = document.getElementById('activatieformulier');
+var registratieformulier = document.getElementById('registratieformulier');
+
+function hide(element) {
+	element.style.display = 'none';
+}
+
+function show(element) {
+	element.style.display = '';
+}
+
+function onSubmit(element, handler) {
+	element.addEventListener('submit', function(event) {
+		event.preventDefault();
+		handler(event.target);
+	});
+}
+
+function post(url, body, handler) {
+	var request = new XMLHttpRequest();
+	request.onreadystatechange = function() {
+		if (request.readyState === XMLHttpRequest.DONE
+				&& request.status === 200) {
+			handler(parseHtml(request.responseText));
+		}
+	};
+	request.open('POST', url, true);
+	request.setRequestHeader('Content-Type',
+			'application/x-www-form-urlencoded; charset=UTF-8');
+	console.log('Sending', body);
+	request.send(body);
+}
+
+function parseHtml(response) {
+	var html = document.createElement('html');
+	html.innerHTML = response;
+	return html;
+}
+
+function proceed(responseHtml, nextScreen) {
+	var foutmelding = responseHtml.querySelector('.label-important');
+	if (foutmelding) {
+		console.log(foutmelding.textContent);
+		errorLabel.textContent = foutmelding.textContent;
+		show(errorLabel);
+	} else {
+		hide(errorLabel);
+		nextScreen(responseHtml);
+	}
+}
+
 document.title = 'Registreer cursist';
-document.querySelector('h1').textContent = 'Cursist registratie...';
-document.querySelector('.label-important').style.display = 'none';
-document.getElementById('activatieformulier').style.display = 'none';
+h1.textContent = 'Cursist registratie...';
+hide(errorLabel);
+hide(activatieformulier);
 
-document
-		.getElementById('registratieformulier')
-		.addEventListener(
-				'submit',
-				function(event) {
-					event.preventDefault();
-					var request = new XMLHttpRequest();
-					var form = document.getElementById('registratieformulier');
-					var registratieGebruikersnaam = form
-							.querySelector('[name=registratieGebruikersnaam]').value;
-					var registratieEmail = form
-							.querySelector('[name=registratieEmail]').value;
-					console.log('Registratie gebruiker',
-							registratieGebruikersnaam, 'email',
-							registratieEmail);
-					request.onreadystatechange = function() {
-						if (request.readyState === XMLHttpRequest.DONE
-								&& request.status === 200) {
-							var response = request.responseText;
-							console.log('Registratie response', response);
-							var html = document.createElement('html');
-							html.innerHTML = response;
-							console.log('HTML', html);
-							var foutmelding = html
-									.querySelector('.label-important');
-							console.log('foutmelding', foutmelding);
-							if (foutmelding) {
-								console.log(foutmelding.textContent);
-								document.querySelector('.label-important').textContent = foutmelding.textContent;
-								document.querySelector('.label-important').style.display = '';
-							} else {
-								document.querySelector('.label-important').style.display = 'none';
-								document.getElementById('registratieformulier').style.display = 'none';
-								document.getElementById('activatieformulier').style.display = '';
-								document.title = 'Activeer cursist';
-								document.querySelector('h1').textContent = 'Cursist activatie...';
-							}
-						}
-					};
-					request.open('POST', '/registreer', true);
-					request.setRequestHeader('Content-Type',
-							'application/x-www-form-urlencoded; charset=UTF-8');
-					request.send("registratieGebruikersnaam="
-							+ encodeURIComponent(registratieGebruikersnaam)
-							+ '&' + "registratieEmail="
-							+ encodeURIComponent(registratieEmail));
-				});
+onSubmit(registratieformulier, function(form) {
+	var registratieGebruikersnaam = form
+			.querySelector('[name=registratieGebruikersnaam]').value;
+	var registratieEmail = form.querySelector('[name=registratieEmail]').value;
+	var body = "registratieGebruikersnaam="
+			+ encodeURIComponent(registratieGebruikersnaam) + '&'
+			+ "registratieEmail=" + encodeURIComponent(registratieEmail);
+	post('/registreer', body, function(responseHtml) {
+		proceed(responseHtml, function(responseHtml) {
+			window.location.hash = 'activeer';
+			hide(registratieformulier);
+			show(activatieformulier);
+			document.title = 'Activeer cursist';
+			h1.textContent = 'Cursist activatie...';
+		});
+	});
+});
 
-document
-		.getElementById('activatieformulier')
-		.addEventListener(
-				'submit',
-				function(event) {
-					event.preventDefault();
-					var request = new XMLHttpRequest();
-					var form = document.getElementById('activatieformulier');
-					var activatieGebruikersnaam = form
-							.querySelector('[name=activatieGebruikersnaam]').value;
-					var activatiecode = form
-							.querySelector('[name=activatiecode]').value;
-					console.log('Activatie gebruiker', activatieGebruikersnaam,
-							'email', activatiecode);
-					request.onreadystatechange = function() {
-						if (request.readyState === XMLHttpRequest.DONE
-								&& request.status === 200) {
-							var response = request.responseText;
-							console.log(response);
-							var html = document.createElement('html');
-							html.innerHTML = response;
-							var foutmelding = html
-									.querySelector('.label-important');
-							if (foutmelding) {
-								document.querySelector('.label-important').textContent = foutmelding.textContent;
-								document.querySelector('.label-important').style.display = '';
-							} else {
-								document.querySelector('.label-important').style.display = 'none';
-								document.getElementById('activatieformulier').style.display = 'none';
-								document.title = 'Account page';
-								document.querySelector('h1').textContent = html
-										.querySelector('h1').textContent;
-							}
-						}
-					};
-					request.open('POST', '/activeer', true);
-					request.setRequestHeader('Content-Type',
-							'application/x-www-form-urlencoded; charset=UTF-8');
-					request.send("activatieGebruikersnaam="
-							+ encodeURIComponent(activatieGebruikersnaam) + '&'
-							+ "activatiecode="
-							+ encodeURIComponent(activatiecode));
-				});
+onSubmit(activatieformulier, function(form) {
+	var activatieGebruikersnaam = form
+			.querySelector('[name=activatieGebruikersnaam]').value;
+	var activatiecode = form.querySelector('[name=activatiecode]').value;
+	var body = "activatieGebruikersnaam="
+			+ encodeURIComponent(activatieGebruikersnaam) + '&'
+			+ "activatiecode=" + encodeURIComponent(activatiecode);
+	post('/activeer', body, function(responseHtml) {
+		proceed(responseHtml, function(responseHtml) {
+			window.location.hash = 'account';
+			hide(activatieformulier);
+			document.title = 'Account page';
+			h1.textContent = responseHtml.querySelector('h1').textContent;
+		});
+	});
+});
